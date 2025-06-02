026b586791080b218452470f0df02baf60eb5875
Yassr atti <cloudec31@gmail.com>
2025-06-02 21:56:34 +0200

    Fix redirection to app: changed it to dashboard to avoid being redirected to /

---
+++ apps/web/app/(app)/dashboard/layout.tsx
@@ -0,0 +1,27 @@
+"use client";
+import type { ReactNode } from "react";
+import "@/web/app/globals.css";
+import { Inter } from "next/font/google";
+
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/main-sidebar/app-sidebar";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export default function RootLayout({ children }: { children: ReactNode }) {
+	return (
+		<html lang="en" suppressHydrationWarning>
+			<body className={inter.className}>
+				<ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+					<SidebarProvider>
+						<AppSidebar variant="inset" />
+						<SidebarInset>
+							<main className="flex-1 p-1">{children}</main>
+						</SidebarInset>
+					</SidebarProvider>
+				</ThemeProvider>
+			</body>
+		</html>
+	);
+}
